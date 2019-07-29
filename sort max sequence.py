e5ad5383587ed8005025953a2d7e460c6032c216
Sam Cladson <43487858+samcladson@users.noreply.github.com>
2019-07-29 09:56:16 +0530

    updated code

--- sort max sequence.py
+++ sort max sequence.py
@@ -5,9 +5,10 @@ list1=[]
 print(val)
 n=len(val)
 for i in range(n-1):
-    if val[i][0]<val[i+1][0]:
-       temp=val[i]
-       val[i]=val[i+1]
-       val[i+1]=temp
+    for j in range(0,2):
+        if val[i][j]<val[i+1][j]:
+           temp=val[i]
+           val[i]=val[i+1]
+           val[i+1]=temp
 maxval = ''.join(str(i) for i in val)
 print(maxval)
