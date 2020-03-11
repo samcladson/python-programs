5a396863dabfef1087ecd270bea390dfcade99ed
Sam Cladson <43487858+samcladson@users.noreply.github.com>
2020-03-11 19:25:27 +0530

    smallest substring program

--- smallest_substring.py
+++ smallest_substring.py
@@ -1,11 +1,12 @@
 from collections import defaultdict as d_dict
-data = d_dict()
-string = list(input())
-count = 0
-for i in string:
-    if i not in data:
-        count+=1
-        data.setdefault(i,count)
-    elif i in data:
-        data[i]=count
-print(min(data.items())[1])
+def shortestSubstring(string):
+    data = d_dict()
+    count = 0
+    for i in string:
+        if i not in data:
+            count+=1
+            data.setdefault(i,count)
+        elif i in data:
+            data[i]=count
+    print(min(data.items())[1])
+shortestSubstring(list(input()))
